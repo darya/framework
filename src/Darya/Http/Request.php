82184a8b8e566c1deb43fdd63d390e45d62ea872
Chris Andrew <chris@hexus.io>
2016-01-27 01:41:48 +0000

    Restored commented code in Request::createFromGlobals().

--- src/Darya/Http/Request.php
+++ src/Darya/Http/Request.php
@@ -223,7 +223,7 @@ class Request {
 		$host = $_SERVER['SERVER_NAME'] ?: $_SERVER['SERVER_ADDR'];
 		$uri  = $_SERVER['REQUEST_URI'];
 		
-		$request = Request::create(/*'//' . $host . $uri*/null, $_SERVER['REQUEST_METHOD'], array(
+		$request = Request::create('//' . $host . $uri, $_SERVER['REQUEST_METHOD'], array(
 			'get'    => $_GET,
 			'post'   => $_POST,
 			'cookie' => $_COOKIE,
